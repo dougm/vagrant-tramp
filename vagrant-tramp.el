34d8909140fe1dd2fb45ac846e2877450dd5e016
Doug MacEachern <dougm@vmware.com>
2014-01-31 13:04:04 -0800

    Function to ssh to a Vagrant box in an ansi-term

--- vagrant-tramp.el
+++ vagrant-tramp.el
@@ -17,13 +17,36 @@
   "Vagrant TRAMP method."
   :group 'tramp)
 
-(defun vagrant-tramp-parse (cmd)
-  "Parse vagrant-tramp-ssh list for vagrant tramp completion"
+(defun vagrant-tramp-list ()
+  "Parse vagrant-tramp-ssh list"
   (with-temp-buffer
-    (shell-command cmd t)
-    (mapcar (lambda (name)
-              (list nil name))
-            (split-string (buffer-string) "\n" t))))
+    (shell-command vagrant-tramp-ssh t)
+    (split-string (buffer-string) "\n" t)))
+
+;; tramp completion functions take a file path argument
+;; where the file must exist or the function is not called.
+;; we ignore the argument here and just use vagrant-tramp-ssh
+;; in vagrant-tramp-list
+(defun vagrant-tramp-parse (_)
+  "Parse vagrant-tramp-ssh list for vagrant tramp completion"
+  (mapcar (lambda (name)
+            (list nil name))
+          (vagrant-tramp-list)))
+
+;;;###autoload
+(defun vagrant-tramp-term (box)
+  "ssh to a Vagrant box in an ansi-term"
+  (interactive
+   (list (ido-completing-read "Vagrant box: "
+                              (vagrant-tramp-list))))
+
+  (let ((name (concat "vagrant ssh " box))
+        (cmd vagrant-tramp-ssh))
+    (with-current-buffer (make-term name cmd nil box)
+      (term-mode)
+      (term-char-mode)
+      (term-set-escape-char ?\C-x)
+      (switch-to-buffer (current-buffer)))))
 
 ;;;###autoload
 (eval-after-load 'tramp
